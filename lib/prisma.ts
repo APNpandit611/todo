710cd09598698cbbb4f825862a734a5b38b3dd9a
APNpandit611 <apnpanditt@gmail.com>
2025-11-22 00:58:01 +0200

    test fixing prisma

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,10 +1,7 @@
-import "dotenv/config";
-import { PrismaPg } from '@prisma/adapter-pg'
-import { PrismaClient } from "@/app/generated/prisma/client";
-const connectionString = `${process.env.DATABASE_URL}`
+import { PrismaClient } from '../app/generated/prisma/client';
+import { PrismaPg } from '@prisma/adapter-pg';
 
-const adapter = new PrismaPg({ connectionString })
-const prisma = new PrismaClient({ adapter })
-
-export { prisma }
-
+const adapter = new PrismaPg({ 
+  connectionString: process.env.DATABASE_URL 
+});
+export const prisma = new PrismaClient({ adapter });
