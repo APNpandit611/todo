ca5f36de4782fbe872732f12a0cb839cde05f7bd
APNpandit611 <apnpanditt@gmail.com>
2025-11-22 00:23:02 +0200

    first commit test

---
+++ components/createUser.ts
@@ -0,0 +1,33 @@
+"use server";
+
+import { prisma } from "@/lib/prisma";
+import { currentUser } from "@clerk/nextjs/server";
+
+const createUser = async () => {
+    try {
+        const user = await currentUser();
+        if (!user) return null;
+
+        const existingUser = await prisma.user.findUnique({
+            where: {
+                id: user.id,
+            },
+        });
+
+        if (!existingUser) {
+            await prisma.user.create({
+                data: {
+                    id: user.id,
+                    email: user.emailAddresses[0].emailAddress,
+                    name: `${user.firstName ?? ""} ${
+                        user.lastName ?? ""
+                    }`.trim(),
+                },
+            });
+        }
+    } catch (error) {
+        console.error("Error creating user:", error);
+    }
+};
+
+export default createUser;
---
+++ components/getUser.ts
@@ -0,0 +1,14 @@
+"use server"
+
+import { prisma } from "@/lib/prisma";
+
+const getUser = async () => {
+    try {
+        const user = await prisma.user.findMany({})
+        return user;
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+export default getUser;
