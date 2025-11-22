4ef2d284864deb5b69ce82d84155d5a0fb4fa5ae
APNpandit611 <apnpanditt@gmail.com>
2025-11-22 12:22:05 +0200

    old version prisma used 6.19

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,10 +1,16 @@
-import "dotenv/config";
-import { PrismaClient } from "@/app/generated/prisma/client";
-import { PrismaPg } from "@prisma/adapter-pg";
-const connectionString = `${process.env.DATABASE_URL}`
+import { PrismaClient } from "@prisma/client";
 
-const adapter = new PrismaPg({ connectionString })
+const prismaClientSingleton = () => {
+  return new PrismaClient()
+}
 
-const prisma = new PrismaClient({ adapter })
+declare const globalThis: {
+  prismaGlobal: ReturnType<typeof prismaClientSingleton>;
+} & typeof global;
 
-export { prisma }
+const prisma = globalThis.prismaGlobal ?? prismaClientSingleton()
+
+export default prisma
+
+if (process.env.NODE_ENV !== 'production') globalThis.prismaGlobal = prisma
+
